8840c81660695ef00be9f1d8ca6543e68d77a12f
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-09-05 22:02:47 +0100

    Created form and added execution endpoint

--- index.php
+++ index.php
@@ -4,9 +4,16 @@
 
 <h1>Rect Playground</h1>
 
+<form action="src/execute.php" method="post">
+    <label>
+        <textarea id="code" name="code" rows="50" cols="100"></textarea>
+    </label>
+    <button type="submit">Execute</button>
+</form>
+
 <?php
-echo "Hello World!";
-?>
+/*echo "Hello World!";
+*/?>
 
 </body>
 </html>
