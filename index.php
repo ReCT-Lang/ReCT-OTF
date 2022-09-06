3ad39223a8673d07f6a5ebb8f42af560516c056e
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-09-06 08:38:11 +0100

    Added option to paste and upload file

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
 
 <h1>Rect Playground</h1>
 
+<p>Paste or upload a file</p>
+
+<input type="file"/>
+
 <form action="src/execute.php" method="post">
     <label>
         <textarea id="code" name="code" rows="50" cols="100"></textarea>
